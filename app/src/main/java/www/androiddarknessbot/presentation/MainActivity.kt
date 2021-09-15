e1800eb0e7096407acd5aebc3a0acf9503be2e69
Tatyana Shkolnik <shkolniktatyana21@gmail.com>
2021-09-15 19:36:03 +0300

    Initial commit

---
+++ app/src/main/java/www/androiddarknessbot/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package www.androiddarknessbot.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import www.androiddarknessbot.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
